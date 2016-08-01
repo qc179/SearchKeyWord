ac03aebac498dc9308600be199ee0412becd99df
qc179 <179313554@qq.com>
2016-08-01 15:38:44 +0800

    Update 区分了三种匹配的模式，增加和调整了输出的细节 增加三种模式：1、只匹配标题 2、只匹配正文 3、同时匹配标题与正文。输出库中对应不同模式的关键词数量，统计文章的字符数。在存储文章的文本文件中默认以第一行位标题，第二行起全部为正文。

--- SearchKeyWord.py
+++ SearchKeyWord.py
@@ -8,11 +8,18 @@ import sys
 def getarticle(filename):
     f = open(filename,'r')
     flist = f.readlines()
+    title = flist[0].decode('gbk').encode('utf8')
+    content = ''
     article = ''
+    for i in flist[1:]:
+        content = content+i
+    content = content.decode('gbk').encode('utf8')
     for i in flist:
         article = article+i
     article = article.decode('gbk').encode('utf8')
-    return article
+    art = {'title':title,'content':content,'article':article}
+    return art
+
 def ismatch(keyword,article):
     y = 2
     er = 'wrong'
@@ -38,21 +45,60 @@ conn = pg2.connect(
     password = '123456',
     host = '192.168.2.128',
     port = '5432')
-cur = conn.cursor()
-sql = r"select kid,word from spam_keyword where filter_pos in (1,2) and word ~ '^(?!.*?\+).*$'"
-cur.execute(sql)
-output = cur.fetchall()
+
+sql1 = r"select kid,word from spam_keyword where filter_pos=0 and word ~ '^(?!.*\+)(?!.*?\))(?!.*?\().*$'"
+cur1 = conn.cursor()
+cur1.execute(sql1)
+output1 = cur1.fetchall()
+print ' filter_pos=0 keyword:',len(output1)
+
+sql2 = r"select kid,word from spam_keyword where filter_pos=1 and word ~ '^(?!.*\+)(?!.*?\))(?!.*?\().*$'"
+cur2 = conn.cursor()
+cur2.execute(sql2)
+output2 = cur2.fetchall()
+print ' filter_pos=1 keyword:',len(output2)
+
+sql3 = r"select kid,word from spam_keyword where filter_pos=2 and word ~ '^(?!.*\+)(?!.*?\))(?!.*?\().*$'"
+cur3 = conn.cursor()
+cur3.execute(sql3)
+output3 = cur3.fetchall()
+print ' filter_pos=2 keyword:',len(output3)
+print ' All key words are not included such as + ( )','\n'
+
+print ' Title length:',len(article['title'].decode('utf8'))
+print ' Content length:',len(article['content'].decode('utf8'))
+print ' Article length:',len(article['article'].decode('utf8')),'\n'
 
 kids = []
-for a in output:
-    if ismatch(a[1],article):
-        print a[0],a[1].decode('utf8')
-        kids.append(a[0])
+print ' Search title ...'
+for a1 in output1:
+    if ismatch(a1[1],article['title']):
+        print '',a1[0],a1[1].decode('utf8')
+        kids.append(a1[0])
     else:
         pass
+print ' Title match kids:',kids,'\n'
 
-print kids
+kids = []
+print ' Search content ...'
+for a2 in output2:
+    if ismatch(a2[1],article['content']):
+        print '',a2[0],a2[1].decode('utf8')
+        kids.append(a2[0])
+    else:
+        pass
+print ' Content match kids:',kids,'\n'
+
+kids = []
+print ' Search article ...'
+for a3 in output3:
+    if ismatch(a3[1],article['article']):
+        print '',a3[0],a3[1].decode('utf8')
+        kids.append(a3[0])
+    else:
+        pass
+print ' Article match kids:',kids,'\n'
+
 conn.close()
-
-start = raw_input('******')
+start = raw_input('********************************************************')
 sys.exit()
